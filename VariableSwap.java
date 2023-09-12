e257e631f93002bdf8e9165522339eff9ec02f58
Tasmiah James <tasmiahtamekajames@gmail.com>
2023-09-12 13:31:30 +0200

    Variable swap
    Swapping two variables using a temp.

---
+++ VariableSwap.java
@@ -0,0 +1,15 @@
+public class VariableSwap {
+    public static void main(String[] args) {
+		
+		String x = "water";
+		String y = "Kool-Aid";
+		String temp;
+		
+		temp = x;
+		x=y;
+		y=temp;
+		
+		System.out.println("x: "+x);
+		System.out.println("y: "+y);
+	}
+}
