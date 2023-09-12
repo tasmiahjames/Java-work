c446da253ba7c7e4bf875ccc739e4aa6ed347bec
Tasmiah James <tasmiahtamekajames@gmail.com>
2023-09-12 15:12:06 +0200

    Objects

---
+++ Objects.java
@@ -0,0 +1,20 @@
+public class Objects {
+
+	public static void main(String[] args) {
+		
+		Car myCar1 = new Car();
+		Car myCar2 = new Car();
+
+		System.out.println(myCar1.make);
+		System.out.println(myCar1.model);
+
+		myCar1.drive();
+		myCar1.brake();
+
+        System.out.println(myCar2.make);
+		System.out.println(myCar2.model);
+
+		myCar2.drive();
+		myCar2.brake();
+	}
+}
