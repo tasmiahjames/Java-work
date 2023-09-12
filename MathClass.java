16b12e3ec9564eadb9fe43b3b1b7136eab521cce
Tasmiah James <tasmiahtamekajames@gmail.com>
2023-09-12 13:54:36 +0200

    Math
    Math class overview.

---
+++ MathClass.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MathClass {
+    public static void main(String[] args) {
+
+        //Math.max(x,y)
+        //Math.min(x,y)
+        //Math.abs(y)
+        //Math.sqrt(x)
+        //Math.round(x)
+        //Math.ceil(x)
+        //Math.floor(x)
+  
+        double x;
+        double y;
+        double z;
+        
+        Scanner scanner = new Scanner(System.in);
+          
+        System.out.println("Enter side x: ");
+        x = scanner.nextDouble();
+        System.out.println("Enter side y: ");
+        y = scanner.nextDouble();
+        
+        z = Math.sqrt((x*x)+(y*y));
+        
+        System.out.println("The hypotenuse is : "+z);
+        
+        scanner.close();   
+       }
+}
