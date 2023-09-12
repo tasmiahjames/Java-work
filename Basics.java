7b4ad9237061c2f514c4b632c7d5c39aee145979
Tasmiah James <tasmiahtamekajames@gmail.com>
2023-09-12 13:23:18 +0200

    Basic Java principles
    - classes
    - main method
    - print()
    - println()
    - escape characters
    - comments

---
+++ Basics.java
@@ -0,0 +1,21 @@
+public class Basics {
+
+    public static void main(String[] args){
+
+        //print vs println statements
+        //println = print + \n
+        System.out.println("test");
+
+        //print = only print
+        System.out.print("test");
+
+        // This is a single line comment
+
+        /*
+         * This is a
+         * multiline comment
+         */
+  
+    }   
+     
+}
