2ba5f49ea18be4df2c3d2fff69333d7dace972ff
Tasmiah James <tasmiahtamekajames@gmail.com>
2023-09-12 13:47:52 +0200

    Arithmetic
    Arithmetic expressions using friends example.

---
+++ Arithmetic.java
@@ -0,0 +1,13 @@
+public class Arithmetic {
+    public static void main(String[] args){
+        // expression = operands & operators
+        // operands = values, variables, numbers, quantity
+        // operators = + - * / % 
+
+        int friends = 10;
+
+        friends--;
+
+        System.out.println(friends);
+    }
+}
