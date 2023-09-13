d43ed10b7e186630804e1f4450c47f9bd6713765
Tasmiah James <tasmiahtamekajames@gmail.com>
2023-09-13 08:57:24 +0200

    Overloaded Constructors

---
+++ OverloadedConstructors.java
@@ -0,0 +1,12 @@
+public class OverloadedConstructors {
+       public static void main(String[] args){
+        
+        Pizza pizza = new Pizza("Thick", "Tomato", "Mozzarella", "Pepperoni");
+
+        System.out.println("Here are the ingredients of your pizza: ");
+        System.out.println(pizza.bread);
+        System.out.println(pizza.sauce);
+        System.out.println(pizza.cheese);
+        System.out.println(pizza.topping);
+       } 
+}
