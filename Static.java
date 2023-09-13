7338f085efedf6793c87f649dcaeb1f65d1c32aa
Tasmiah James <tasmiahtamekajames@gmail.com>
2023-09-13 10:52:42 +0200

    Static Keyword

---
+++ Static.java
@@ -0,0 +1,13 @@
+public class Static {
+    
+    public static void main(String[] args){
+
+        //Friend friend1 = new Friend("Spongebob");
+        //Friend friend2 = new Friend("Patrick");
+        //Friend friend3 = new Friend("Squidward");
+        //Friend friend4 = new Friend("Sandy");
+        
+        Friend.displayFriends();
+
+    }
+}
